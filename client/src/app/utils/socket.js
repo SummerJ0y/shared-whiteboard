4104ed5cfce58db07f833ed77730852e15f7fdbf
Yingjia Liu <yliu4033@gmail.com>
2025-04-01 15:27:56 -0500

    some bugs

--- client/src/app/utils/socket.js
+++ client/src/app/utils/socket.js
@@ -3,6 +3,6 @@ import { io } from "socket.io-client";
 
 // Only connect once (on first import)
 // const socket = io("http://localhost:5001");
-const socket = io("http://10.0.0.230:5001")
+const socket = io("http://10.0.0.207:5001")
 
 export default socket;
