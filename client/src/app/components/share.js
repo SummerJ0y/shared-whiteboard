20317a10ea6e2b4e0b7b16ff7bb7eec2795d00ae
Yingjia Liu <yliu4033@gmail.com>
2025-04-13 01:16:30 -0500

    share

--- client/src/app/components/share.js
+++ client/src/app/components/share.js
@@ -29,13 +29,33 @@ export default function SharePopup({ setShareWindow }) {
                       className={styles.urlBar}
                     />
                     <button className={styles.copyButton} onClick={handleCopy}>Copy</button>
-                    
+
                     {showCopyMessage && (
                         <div className={styles.copyMessage}>
                             Copied to your clipboard!
                         </div>
                     )}
                 </div>
+
+                <div style={{fontSize: '18px', marginTop: '40px'}}>General Access Setting</div>
+                                
+                <div style={{ display: 'flex', alignItems: 'center', marginTop: '15px', width: '90%' }}>
+                    <span style={{ marginRight: '10px', fontSize: '16px' }}>People with the link can:</span>
+                    <select style={{ flexGrow: 1, padding: '6px', borderRadius: '5px', fontSize: '16px' }}>
+                        <option>Can only view</option>
+                        <option>Can View and edit</option>                        
+                        <option>Can't view unless authorized</option>
+                    </select>
+                </div>
+
+                <div style={{ display: 'flex', alignItems: 'center', marginTop: '10px', width: '90%' }}>
+                    <span style={{ marginRight: '10px', fontSize: '16px' }}>Authorized user can:</span>
+                    <select style={{ flexGrow: 1, padding: '6px', borderRadius: '5px', fontSize: '16px' }}>
+                        <option>Can only view</option>
+                        <option>Can view and edit</option>
+                    </select>
+                </div>
+
 
                 <div className={styles.closeButton}>
                     <Image
